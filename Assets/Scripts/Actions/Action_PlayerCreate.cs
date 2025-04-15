98a38981b456c66a2a634f5a2816aced3202f52b
Memnyatta <shumaylov2000@gmail.com>
2025-04-15 19:08:02 +0300

    Update Action_PlayerCreate.cs

--- Assets/Scripts/Actions/Action_PlayerCreate.cs
+++ Assets/Scripts/Actions/Action_PlayerCreate.cs
@@ -15,7 +15,8 @@ public static class Action_PlayerCreate
     {
 
         GameObject Object = parameters.Object;
-
+        List<Vector2Int> zone = parameters.CellsCoordinates;
+   
         UnitPlacementHolderObj = GameObject.Find(UnitPlacementHolderStr);
         Vector3 holdPos = Vector3.zero; if (UnitPlacementHolderObj != null) { holdPos = UnitPlacementHolderObj.transform.position; }
 
